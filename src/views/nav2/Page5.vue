7c266654a612557651513a2f3bcfb9e519edcaa9
happyplus-chen <happyplus_chen@163.com>
2017-05-22 23:28:48 +0800

    test1111

--- src/views/nav2/Page5.vue
+++ src/views/nav2/Page5.vue
@@ -1,4 +1,99 @@
 <template>
-	<section>page5...
-	</section>
+	<el-form  label-width="180px">
+		<el-col :span="22">
+			<el-form-item label="缺陷名称">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目名称">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+		<el-col :span="22">
+			<el-form-item label="项目编号">
+				<el-input type="textarea"></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+				<el-col :span="11">
+			<el-form-item label="项目编号">
+				<el-input></el-input>
+			</el-form-item>
+		</el-col>
+	</el-form>
 </template>
+<script>
+export default {
+
+}
+</script>
