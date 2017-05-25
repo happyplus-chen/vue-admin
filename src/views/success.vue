a162113281ba4043d8f8758e4c501f7b38e6440f
happyplus-chen <happyplus_chen@163.com>
2017-05-25 22:12:34 +0800

    ssss

---
+++ src/views/success.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="center">
+        <i class="fa fa-check-circle-o"
+           style="font-size: 50px;color: green;"></i><span style="font-size: 25px;color: green;">  提交成功！</span>
+        <p style="color:#8391a5">{{num}} 秒后将自动跳转到问题单列表页</p>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            num: 5
+        }
+    },
+    methods: {
+        rerouter() {
+            let interval = setInterval(() => {
+                this.num--;
+                if (this.num == 0) {
+                    clearInterval(interval);
+                    this.$router.push({ path: '/table' });
+                }
+            }, 1000);
+        }
+    },
+    mounted() { this.rerouter() }
+
+}
+
+</script>
+
+<style scoped>
+.center {
+    text-align: center;
+    margin-top: 100px
+}
+</style>
